4b88f1c72bf655e89957e83347969d22330055ff
Wellington <wellingtonmachioni@gmail.com>
2025-11-27 20:45:10 -0300

    feat(form): apply validation with jQuery

--- script.js
+++ script.js
@@ -6,4 +6,48 @@ $(() => {
     });
     bsCollapse.hide();
   });
+
+  $("#contact-form").validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 3,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      message: {
+        required: true,
+        minlength: 10,
+      },
+    },
+    messages: {
+      name: {
+        required: "Por favor, insira seu nome",
+        minlength: "O nome deve ter pelo menos 3 caracteres",
+      },
+      email: {
+        required: "Informe seu email",
+        email: "Digite um email válido",
+      },
+      message: {
+        required: "Digite uma mensagem",
+        minlength: "A mensagem deve conter no mínimo 10 caracteres",
+      },
+    },
+    invalidHandler: function (_event, validator) {
+      if (validator.errorList.length) {
+        let el = validator.errorList[0].element;
+        el.focus(); // força o foco no primeiro campo inválido
+        // rolar até o campo
+        $("html, body").animate(
+          {
+            scrollTop: $(el).offset().top - 150,
+          },
+          500
+        );
+      }
+    },
+  });
 });
